633ed2dd09f961f052efbb0b3488de916b5f34ad
BrotherMo1 <155409498+BrotherMo1@users.noreply.github.com>
2025-01-08 08:45:03 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let baseApi = "https://opentdb.com/api.php?amount=10";
+const easyApi = baseApi + "&difficulty=easy";
+const midApi = baseApi + "&difficulty=medium";
+const hardApi = baseApi + "&difficulty=hard";
+let disQ;
+let disO;
+let difficulty;
+
+async function grabUrl()
+{
+    try
+    {
+    const response = await fetch(baseApi);
+    const data = await response.json();
+    console.log(data);
+
+    difficulty = document.getElementById("difficulty"); 
+    difficulty.innerHTML = `<span>Difficulty:</span>` + `<span>${data.results[0].difficulty}</span>` + `<br>` + `<br>`;
+
+    disQ = document.getElementById("disQ"); 
+    disQ.innerHTML = `<span>${data.results[0].question}</span>`  + `<br>`  + `<br>`;
+    
+    disO = document.getElementById("disO"); 
+    disO.innerHTML = `<span>${data.results[0].correct_answer}</span>`+ `<span>${data.results[0].incorrect_answers} </span>`;
+    }
+
+    catch
+    {
+        console.error("Error Fetching Api");
+    }
+
+}
+
+grabUrl();
