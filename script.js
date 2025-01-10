aed14aa46fd2c60bcc8075d0abf5004927ff01fd
BrotherMo1 <155409498+BrotherMo1@users.noreply.github.com>
2025-01-10 09:33:56 -0800

    update jan 10 code

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ let trivApi = "https://opentdb.com/api.php?amount=10";
 const easyApi = trivApi + "&difficulty=easy";
 const midApi = trivApi + "&difficulty=medium";
 const hardApi = trivApi + "&difficulty=hard";
+let apiKey = "vl3EiNGXGZACABgTOliTXjU9okdiloezxhaMKUbYUjrxY05suMB9fibD";
 let disQ;
 let disO;
 let difficulty;
 
-// pexels api key: vl3EiNGXGZACABgTOliTXjU9okdiloezxhaMKUbYUjrxY05suMB9fibD
+
 window.onload = function ()
 {
     grabUrl();
+    // imgSearch();
 }
 async function grabUrl()
 {
@@ -41,7 +43,7 @@ async function qAs (data)
         let quest = document.createElement('h3');
         quest.innerHTML = data.results[i].question;
         questions.appendChild(quest);
-
+ 
         //`<p>${data.results[i].question}</p>`  + `<br>`  + `<br>`;
 
         let answers = document.createElement('div');
@@ -57,5 +59,56 @@ async function qAs (data)
         container.appendChild(questions);
         container.appendChild(answers);
     }
+    imgDelivery(data);
+}
+
+// let query = 'apple';
+// async function imgSearch(query){
+//     // fetch the data from api
+//     try {
+//         const data=await
+// fetch(`https://api.pexels.com/v1/search?query=${query}?orientation=square
+// `,
+//             {
+//                 method: "GET",
+//                 headers: {
+//                     Accept: "application/json",
+//                     Authorization: apiKey,       
+//                 },
+//             });
+//             const response=await data.json(); 
+//             console.log(response);
+//     } catch (error) {
+//         console.error('Error fetching image:', error);
+
+//     }
+
+// }
+
+async function fetchPexelsData(data, search) {
+    const url = "https://api.pexels.com/v1/search?per_page=1&query=" + search;
+    const headers = {
+        "Authorization": apiKey
+    };
+
+    try {
+        const response = await fetch(url, { headers });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data1 = await response.json();
+        console.log(data1);
+        return data1;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+async function imgDelivery (data)
+{
+
+    fetchPexelsData(data.results[0].correct_answer + data.results[0].question);
 }
 
