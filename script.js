e31ad5405182378784aef0c27918d8affcfa6904
BrotherMo1 <155409498+BrotherMo1@users.noreply.github.com>
2025-01-11 20:11:05 -0800

    readjust img

--- script.js
+++ script.js
@@ -59,7 +59,10 @@ async function qAs (data, data1)
         container.appendChild(questions);
         container.appendChild(answers);
     }
-
+    
+    let img = document.createElement('div')
+    img.innerHTML = `<img src=${data1.photos[0].src.medium}> alt="image not available"`;
+    
     imgDelivery(data);
 }
 
